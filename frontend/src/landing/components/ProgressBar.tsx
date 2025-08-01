ac1eaca2179959c4545f4b458028ae39e5b026bf
Franber <franber.cba@gmail.com>
2025-08-01 14:44:55 -0400

    hotfix percentage behavior

--- frontend/src/landing/components/ProgressBar.tsx
+++ frontend/src/landing/components/ProgressBar.tsx
@@ -8,7 +8,7 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({ percentage }) => (
   <div className='w-full max-w-md mx-auto bg-gray-300 rounded-full h-4 overflow-hidden my-4'>
     <div
       className='bg-orange-500 h-4 rounded-full transition-all duration-500'
-      style={{ width: `${percentage}%` }}
+      style={{ width: `${percentage ?? 0}%` }}
     />
   </div>
 );
